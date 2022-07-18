25d07fdb2a1fce69ccf36b4fb09fa6b0b88c65eb
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-07-18 23:52:06 +0100

    Add files via upload

--- article.js
+++ article.js
@@ -3,7 +3,7 @@ const popUp2 = document.querySelector(".pop-up-2");
 const shareBtn = document.querySelector(".share-btn");
 const shareBtn2 = document.querySelector(".share-btn-2");
 const shareIcon = document.querySelector(".fa-share");
-const shareIcon2 = document.querySelector(".fa-share-2");
+const shareIcon2 = document.getElementById("fa-share-2");
 
 shareBtn.addEventListener("click", () => {
   popUp.classList.toggle("active");
