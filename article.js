1cceceb6aae8bd17eba94b4be31719ca3ed81f17
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-07-18 23:49:15 +0100

    Add files via upload

---
+++ article.js
@@ -0,0 +1,18 @@
+const popUp = document.querySelector(".pop-up");
+const popUp2 = document.querySelector(".pop-up-2");
+const shareBtn = document.querySelector(".share-btn");
+const shareBtn2 = document.querySelector(".share-btn-2");
+const shareIcon = document.querySelector(".fa-share");
+const shareIcon2 = document.querySelector(".fa-share-2");
+
+shareBtn.addEventListener("click", () => {
+  popUp.classList.toggle("active");
+  shareIcon.classList.toggle("active");
+  shareBtn.classList.toggle("active");
+});
+
+shareBtn2.addEventListener("click", () => {
+  popUp2.classList.toggle("active");
+  shareIcon2.classList.toggle("active");
+  shareBtn2.classList.toggle("active");
+});
